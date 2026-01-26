bca595549931b75a0c8fab7019941fafe5f7dd4c
liambamufaya <196014763+liambamufaya@users.noreply.github.com.>
2026-01-26 18:32:36 +0200

    Add basic operator functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    return a + b
+}
+
+function subtract(a,b){
+    return b -a
+}
+
+function multiplication(a,b){
+    return a* b
+}
+
+function division(a,b ){
+    return b / a
+}
