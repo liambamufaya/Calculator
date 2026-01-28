5693dfa742a6076c052f4133cee40ac90826d491
liambamufaya <196014763+liambamufaya@users.noreply.github.com.>
2026-01-28 21:43:52 +0200

    Make the screen work after pushing buttons

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
+let op1;
+let op2;
+let operator;
+
+
+
 function add(a,b){
-    return a + b
+    return a + b;
 }
 
 function subtract(a,b){
-    return b -a
+    return b - a;
 }
 
-function multiplication(a,b){
-    return a* b
+function multiply(a,b){
+    return a* b;
 }
 
-function division(a,b ){
-    return b / a
+function divide(a,b ){
+    return b / a;
 }
+
+
+const operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+};
+
+function operate(op1,op2,operand){
+    operations[operand]?.(op1,op2);
+
+}
+
+const digit = document.querySelector("#digit")
+
+const button = document.querySelector("#container")
+button.addEventListener('click', (e)=>{
+    if(e.target.classList.contains("btn")){
+        digit.textContent += e.target.textContent 
+    }
+});
+
+
