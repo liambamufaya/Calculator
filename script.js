a3ac82b1bc2e5c2e99a8ec73caca8f86724eaccc
liambamufaya <196014763+liambamufaya@users.noreply.github.com.>
2026-02-01 11:20:20 +0200

    feat: Implement calculator operations and state handling

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let op1;
-let op2;
-let operator;
+// Storing the first and second and the operator
+let firstNumber = null;
+let secondNumber = null;
+let currentOperator = null;
+let result = null;
+let shouldResetScreen = false;
 
-
-
+// Mathematical operations
 function add(a,b){
     return a + b;
 }
 
 function subtract(a,b){
-    return b - a;
+    return a - b;
 }
 
 function multiply(a,b){
@@ -17,9 +19,11 @@ function multiply(a,b){
 }
 
 function divide(a,b ){
-    return b / a;
+    if (b === 0) return "Error";
+    return a / b;
 }
 
+//Getting the function based on the operator
 
 const operations = {
     '+': add,
@@ -28,22 +32,73 @@ const operations = {
     '/': divide,
 };
 
-function operate(op1,op2,operand){
-    operations[operand]?.(op1,op2);
-
+function operate(a,b,operand){
+  return operations[operand]?.(a,b);
 }
 
-const digit = document.querySelector("#digit")
 
-const button = document.querySelector("#container")
-button.addEventListener('click', (e)=>{
-    if(e.target.classList.contains("btn")){
-        digit.textContent += e.target.textContent 
+// Variable storing text on screen
+const screen = document.querySelector("#screen");
+const container = document.querySelector("#container");
+
+container.addEventListener('click', (e)=>{
+    const target = e.target;
+
+    
+    // Clear the screen when C button ic clicked
+    if (target.classList.contains("clear")){
+        screen.textContent = "";
+        firstNumber = null;
+        secondNumber = null;
+        currentOperator = null;
+        result = null;
+
     }
 
-    if (e.target.classList.contains("clear")){
-        digit.textContent = ""
+     // Show clicked buttons
+    if(target.classList.contains("number")){
+        if(shouldResetScreen) {
+            screen.textContent = "";
+            shouldResetScreen = false;
+
+        }
+        screen.textContent += target.textContent        
     }
+        
+    // When operator is pressed
+    if (target.classList.contains("operator")){
+
+        if (screen.textContent === "") return;
+
+        if (firstNumber === null && currentOperator === null){
+            firstNumber= Number(screen.textContent)
+            currentOperator = target.textContent;
+            screen.textContent = "";
+            shouldResetScreen = true;
+        }
+        else {
+            secondNumber= Number(screen.textContent);
+            screen.textContent = "";
+            result = operate(firstNumber,secondNumber,currentOperator)
+            currentOperator = e.target.textContent
+            firstNumber = result;
+            screen.textContent = firstNumber;
+            shouldResetScreen = true;
+           
+        }
+    }
+    
+
+        // When equal sign is pressed
+        if (target.classList.contains("equal")){
+            if (currentOperator === null || screen.textContent === "") return;
+            secondNumber =  Number(screen.textContent);
+            result =  operate(firstNumber,secondNumber,currentOperator);
+            
+            screen.textContent = result;
+            firstNumber = result;
+            currentOperator = null;
+            shouldResetScreen = true;
+
+        }
 });
-
-
